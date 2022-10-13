c4cb14c579be281226814322e61977b740dd53d0
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-14 00:05:35 +0200

    build

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -15,8 +15,6 @@ export const formatXksAmount = (amount, digits = 2, round = false) => {
 }
 
 export function checkIfNewBlock(prevBlock, newBlock) {
-    console.log('PREV', prevBlock.height)
-    console.log('NEW', newBlock.height)
     if (prevBlock.height < newBlock.height) {
         return true
     }
