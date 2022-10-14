ae9f4de9851fee68a0974e90d4c9e980a6aa88f6
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-14 12:17:35 +0200

    clean

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,21 +4,29 @@
 </script>
 
 <nav class="container">
-    <a href="/"><h1>Explorer</h1></a>
-    <div>
+    <div class="navigation">
+        <a href="/"><h1>Explorer</h1></a>
         <Search/>
     </div>
 </nav>
 
 <style lang="scss">
+
   nav {
     display: flex;
     justify-content: space-between;
     align-items: center;
 
     a {
-      margin: 1rem;
       text-decoration: none;
     }
+  }
+
+  .navigation {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1rem 1rem;
   }
 </style>
