4dc266911a818dd1a261771615e4a493547a43ac
Lukas <36674091+Swepool@users.noreply.github.com>
2022-10-14 13:06:38 +0200

    details

--- src/lib/components/Blockchain.svelte
+++ src/lib/components/Blockchain.svelte
@@ -2,15 +2,23 @@
     import {explorerData} from "$lib/stores/data";
     import {fade} from 'svelte/transition'
     import { Moon } from 'svelte-loading-spinners'
+    import Time from "svelte-time";
+
+    console.log($explorerData.blocks.slice(0,6))
 </script>
 
 <div class="container wrapper">
     <div class="block">
+        <div></div>
         <Moon color="var(--title-color)" size="30"/>
+        <div></div>
     </div>
     {#each $explorerData.blocks.slice(0,6) ?? [] as block, i}
         <div in:fade|local class="block">
-            <p>{block.height}</p>
+            <h4>{block.height}</h4>
+            <p>{block.num_txes} txs</p>
+            <p>{(block.reward / 100000).toFixed(2)}</p>
+            <p class="time"><Time relative timestamp={block.timestamp * 1000}/></p>
         </div>
     {/each}
 </div>
@@ -32,14 +40,24 @@
 
     .block {
       display: flex;
+      padding: 1rem;
+      justify-content: space-between;
       min-width: 128.5px;
       height: 128.5px;
       flex-direction: column;
       align-items: center;
-      justify-content: center;
       border: var(--card-border);
       background-color: var(--card-background);
       border-radius: 5px;
       grid-column: span 2 / span 2;
+
+      p {
+        font-size: 0.75rem;
+      }
+
+      .time {
+        font-size: 0.6rem;
+        opacity: 50%;
+      }
     }
 </style>
