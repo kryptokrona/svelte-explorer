78b0283ba343c5f170639c7beaf598bbc5019179
appelskrutt34 <ebba1430@hotmail.com>
2022-12-22 12:11:16 +0100

    changed api in transaction-pool also

--- src/lib/stores/transaction-pool.js
+++ src/lib/stores/transaction-pool.js
@@ -1,21 +1,21 @@
-import {writable} from "svelte/store";
+import { writable } from 'svelte/store';
 
-export const txPool = writable([])
+export const txPool = writable([]);
 
 export const fetchMempool = async () => {
-    const response = await fetch('https://swepool.org/rpc/json_rpc', {
-        method: 'POST',
-        cache: 'no-cache',
-        redirect: 'follow',
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify({
-            jsonrpc: "2.0",
-            id: "test",
-            method: "f_on_transactions_pool_json",
-            params: {}
-        })
-    });
-    let data = await response.json();
-    txPool.set(data.result.transactions)
-}
-fetchMempool()
+	const response = await fetch('https://blocksum.org/api/json_rpc', {
+		method: 'POST',
+		cache: 'no-cache',
+		redirect: 'follow',
+		referrerPolicy: 'no-referrer',
+		body: JSON.stringify({
+			jsonrpc: '2.0',
+			id: 'test',
+			method: 'f_on_transactions_pool_json',
+			params: {}
+		})
+	});
+	let data = await response.json();
+	txPool.set(data.result.transactions);
+};
+fetchMempool();
